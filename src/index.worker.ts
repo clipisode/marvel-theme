a30396159ccd194f0b8b09c4598d7d2814b9efc0
Brian Alvey <github@subs.frankensite.com>
2021-05-24 22:37:21 -0700

    new reply button label

--- src/index.worker.ts
+++ src/index.worker.ts
@@ -7,7 +7,9 @@ export const getCustomDataDescriptors: GetCustomDataDescriptorsFn = () => {
 export const siteData: SiteData = {
   customCssKey: "site.css",
   brandColor: "#7df42e",
-  introScreen: {},
+  introScreen: {
+    recordButtonLabel: "Record a M01 video reply",
+  },
   nameScreen: {},
   emailScreen: {},
   closedScreen: {},
