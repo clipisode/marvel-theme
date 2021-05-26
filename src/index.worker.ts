f7fc4149ee8908ed7a30ae642fa56661520e04ce
Brian Alvey <github@subs.frankensite.com>
2021-05-26 14:01:59 -0700

    more test values

--- src/index.worker.ts
+++ src/index.worker.ts
@@ -9,6 +9,9 @@ export const siteData: SiteData = {
   introScreen: {
     recordButtonLabel: "Record a video reply",
   },
+  introScreenDesktop: {
+    markup: `<p className="bg-black">black</p><p className="bg-white">white</p><div className="border m-8">QR_CODE</div><p className="bg-black">black</p>`,
+  },
   nameScreen: {},
   emailScreen: {},
   closedScreen: {},
