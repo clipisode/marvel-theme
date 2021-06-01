8884c59fdc72b33121765f229529b023aedf2ce8
Brian Alvey <github@subs.frankensite.com>
2021-06-01 09:46:30 -0700

    test data

--- src/index.worker.ts
+++ src/index.worker.ts
@@ -7,9 +7,33 @@ export const getCustomDataDescriptors: GetCustomDataDescriptorsFn = () => {
 export const siteData: SiteData = {
   customCssKey: "styles.css",
   introScreen: {
-    recordButtonLabel: "Record a video reply",
+    recordButtonLabel: "Excelsior!",
   },
-  nameScreen: {},
-  emailScreen: {},
-  closedScreen: {},
+  successScreen: {
+    markup: `
+            <div class="flex flex-grow flex-col items-center justify-between">
+                <div>
+                    <p class="mt-12 mb-12 mx-3 text-center">Avengers assemble! Your video request for <span class="whitespace-nowrap"><b>INVITATION_DISPLAY_NAME</b> was sent.</span></p>
+                    <div class="hidden iosOnly text-center">
+                        <div>
+                          <a class="downloadButton" href="https://apps.apple.com/us/app/clipisode-easy-video-collabs/id1475959226#?platform=iphone">Download the app</a>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div class="hidden iosOnly">
+                        <div class="mb-24">
+                          <a href="https://apps.apple.com/us/app/clipisode-easy-video-collabs/id1475959226#?platform=iphone">
+                            <img class="h-10 mx-auto" src="THEME_FILE_ROOT/app-store.svg" />
+                          </a>
+                        </div>
+                    </div>
+                    <div class="hidden androidOnly mb-16">
+                        <p class="bg-yellow-100 text-center text-sm mx-3 mb-4">Clipisode is only available for <span class="whitespace-nowrap">iPhone and iPad</span> right now.</p>
+                        <p class="text-center text-sm mx-3 mb-4"><a class="text-blue-500 underline" href="care@clipisode.com">Email us</a> if you want to talk about Clipisode for Android.</p>
+                    </div>
+                </div>
+            </div>
+        `,
+  },
 };
